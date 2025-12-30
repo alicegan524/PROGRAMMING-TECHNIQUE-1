1b1afcb81617edcfc9c9489ff095febadfed8c71
GAN MEI LEE <alicegan524@gmail.com>
2025-12-30 15:05:50 +0800

    Add files via upload

---
+++ PT2 LAB 3.cpp
@@ -0,0 +1,18 @@
+// Name: Gan Mei Lee	Matric Numer: A25CS0225		Date: 30/12/2025 
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	const int j=5;
+	int E [j];
+	for (int i=0; i<j; i++)
+	{
+		if (i%2==0)
+			cout << "XXXXXX" << endl;
+		else 
+			cout << "XX" << endl;
+	}
+	return 0;
+}
